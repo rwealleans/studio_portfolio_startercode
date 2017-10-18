7f7c72bb7dd6ba72c917a100be6d2eed8681a55a
Peter Jablonski <pwjablonski@gmail.com>
2017-10-18 11:59:31 -0400

    Create script.js

---
+++ script/script.js
@@ -0,0 +1,8 @@
+//Unit 1 Portfolio with Firebase
+// Use Firebase to store the images links and info for your projects. 
+// Use Jquery to dynamically display the information from firebse. 
+
+// *************************** YOUR CODE BELOW *******************************
+//******************TEST EARLY AND OFTEN USING console.log() ******************
+//****************** SERIOUSLY TEST USING console.log()!!! ******************
+
